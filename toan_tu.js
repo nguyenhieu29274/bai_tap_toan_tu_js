e6e05039e2084206b131c673f99ea233af884c65
nguyenhieu29274 <112528811+nguyenhieu29274@users.noreply.github.com>
2022-09-07 21:14:41 +0700

    toan_tu_js.html

---
+++ toan_tu.js
@@ -0,0 +1,9 @@
+var a = 12;
+var b = 10;
+
+if ( a == b){
+    alert('a va b bang nhau');
+}
+else {
+    alert('a va b khac nhau');
+}
---
+++ toantu2.js
@@ -0,0 +1,4 @@
+let i = 10;
+let f = 20.5;
+let b = true;
+let s = 'ha noi';
